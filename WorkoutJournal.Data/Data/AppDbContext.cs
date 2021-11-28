a1ce128b7cd2b4320c3838ca6053811179800641
durid-ah <ahmad.durid96@gmail.com>
2021-11-28 01:22:58 -0700

    Refactored the dbcontext code to have a secondary file and  removed unnecessary steps

--- WorkoutJournal.Data/Data/AppDbContext.cs
+++ WorkoutJournal.Data/Data/AppDbContext.cs
@@ -1,8 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using Newtonsoft.Json;
 using WorkoutJournal.Data.Data.Models;
 
 namespace WorkoutJournal.Data.Data;
@@ -15,42 +12,8 @@ public class AppDbContext: DbContext
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
-        builder.ApplyConfiguration(new ExerciseConfiguration());
-    }
-}
-
-public class ExerciseConfiguration : IEntityTypeConfiguration<ExerciseTemplate>
-{
-    public void Configure(EntityTypeBuilder<ExerciseTemplate> builder)
-    {
-        builder.Property(et => et.TargetSets)
+        builder.Entity<ExerciseTemplate>()
+            .Property(x => x.TargetSets)
             .HasJsonConversion();
     }
 }
-
-public static class ValueConversionExtensions
-{
-    public static PropertyBuilder<T> HasJsonConversion<T>(this PropertyBuilder<T> propertyBuilder) where T : class, new()
-    {
-        ValueConverter<T, string> converter = new ValueConverter<T, string>
-        (
-            v => JsonConvert.SerializeObject(v),
-            v => JsonConvert.DeserializeObject<T>(v) ?? new T()
-        );
-
-        ValueComparer<T> comparer = new ValueComparer<T>
-        (
-            (l, r) => JsonConvert.SerializeObject(l) == JsonConvert.SerializeObject(r),
-            v => v == null ? 0 : JsonConvert.SerializeObject(v).GetHashCode(),
-            v => JsonConvert.DeserializeObject<T>(JsonConvert.SerializeObject(v))
-        );
-
-        propertyBuilder.HasConversion(converter);
-        propertyBuilder.Metadata.SetValueConverter(converter);
-        propertyBuilder.Metadata.SetValueComparer(comparer);
-        propertyBuilder.HasColumnType("jsonb");
-
-        return propertyBuilder;
-    }
-}
-
---
+++ WorkoutJournal.Data/Data/ValueConversionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Newtonsoft.Json;
+
+namespace WorkoutJournal.Data.Data;
+
+public static class ValueConversionExtensions
+{
+    public static PropertyBuilder<T> HasJsonConversion<T>(this PropertyBuilder<T> propertyBuilder) where T : class, new()
+    {
+        ValueConverter<T, string> converter = new (
+            v => JsonConvert.SerializeObject(v),
+            v => JsonConvert.DeserializeObject<T>(v) ?? new T()
+        );
+
+        ValueComparer<T> comparer = new (
+            (l, r) => JsonConvert.SerializeObject(l) == JsonConvert.SerializeObject(r),
+            v => v == null ? 0 : JsonConvert.SerializeObject(v).GetHashCode(),
+            v => JsonConvert.DeserializeObject<T>(JsonConvert.SerializeObject(v))
+        );
+
+        propertyBuilder.HasConversion(converter);
+        propertyBuilder.Metadata.SetValueConverter(converter);
+        propertyBuilder.Metadata.SetValueComparer(comparer);
+        propertyBuilder.HasColumnType("jsonb");
+
+        return propertyBuilder;
+    }
+}
+
