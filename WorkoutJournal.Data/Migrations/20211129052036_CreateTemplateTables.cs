b230b97a7eedbc10bf0916e4839589b516dd3952
durid-ah <ahmad.durid96@gmail.com>
2021-11-28 22:23:45 -0700

    creating the first migration

---
+++ WorkoutJournal.Data/Migrations/20211129052036_CreateTemplateTables.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkoutJournal.Data.Migrations
+{
+    public partial class CreateTemplateTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WorkoutTemplate",
+                columns: table => new
+                {
+                    WorkoutTemplateId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastUpdated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkoutTemplate", x => x.WorkoutTemplateId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExerciseTemplates",
+                columns: table => new
+                {
+                    ExerciseTemplateId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    WeightUnit = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RepType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WorkoutTemplateId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TargetSets = table.Column<string>(type: "jsonb", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExerciseTemplates", x => x.ExerciseTemplateId);
+                    table.ForeignKey(
+                        name: "FK_ExerciseTemplates_WorkoutTemplate_WorkoutTemplateId",
+                        column: x => x.WorkoutTemplateId,
+                        principalTable: "WorkoutTemplate",
+                        principalColumn: "WorkoutTemplateId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExerciseTemplates_WorkoutTemplateId",
+                table: "ExerciseTemplates",
+                column: "WorkoutTemplateId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExerciseTemplates");
+
+            migrationBuilder.DropTable(
+                name: "WorkoutTemplate");
+        }
+    }
+}
